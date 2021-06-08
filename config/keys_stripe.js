408f7e61994184443486492fc3cbe6dc8b7b7c76
Rishika0109 <82610555+Rishika0109@users.noreply.github.com>
2021-06-08 18:19:57 +0530

    Add files via upload

---
+++ config/keys_stripe.js
@@ -0,0 +1,6 @@
+module.exports={
+    stripe:{
+        PUBLISHABLE_KEY:'pk_test_51IyWvWSBus84JHp3FF2d9wrzVUVoNQpcLvMqL06ibfh7yVgcK7sJ7Txxl9l7sG8wsXBpsuve988GFvhrSdkXjtvs00zOhzuDiZ',
+SECRET_KEY:'sk_test_51IyWvWSBus84JHp3yx38LYmP99jRqag35aakEBCFq46vMyGgdmSgdjIBJh93ncuNHyE69hdkQmQ8OBOGbaUIkDoE00ShqtMkDz'
+    }
+};
