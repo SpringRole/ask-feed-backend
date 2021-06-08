648a25755d4d937d4afc8fceebb65aff8842cc4a
Rishika0109 <82610555+Rishika0109@users.noreply.github.com>
2021-06-08 17:20:28 +0530

    Add files via upload

---
+++ config/passport-setup.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys= require('./keys');
+passport.use(new GoogleStrategy({
+    clientID:keys.google.clientID ,
+    clientSecret: keys.google.clientSecret,
+    callbackURL: "/auth/google/callback"
+  },(accessToken, refreshToken, profile, done)=> {
+    User.findOne({googleId: profile.id})
+    .then((currentuser)=>{
+      if(currentuser){
+        console.log('presnt');
+        done(null, currentuser);
+      }
+      else{
+        const user = new User({
+          googleId: profile.id
+        });
+        user.save()
+        .then((result)=>{
+          console.log('db created');
+          done(null, result);
+        })
+      }
+      
+    });
+  }
+));
