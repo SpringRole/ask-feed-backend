21f3fbf250e5345c8082a85ecb39ba3a636d8710
Rishika0109 <82610555+Rishika0109@users.noreply.github.com>
2021-06-08 17:23:05 +0530

    Delete user.js

--- db/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const db = new Schema({
-    googleId: {
-        type: String, required: true
-    }
-});
-const User = mongoose.model('User', db);
-module.exports = User;
