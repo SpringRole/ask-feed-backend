bd44de775840997d8bafc19dd0e8ab7f895e7569
Rishika0109 <82610555+Rishika0109@users.noreply.github.com>
2021-06-08 18:24:45 +0530

    Update oauth.js

--- oauth.js
+++ oauth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const passport = require('passport');
-const router=require('./routes/auth-routes');
+const router=require('./routes/auth_routes');
+const router1=require('./routes/payment_routes');
 const keys= require('./config/keys');
 const passportSetup = require('./config/passport-setup');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app=express();
 app.use('/auth',router);
+app.use('/payment',router1);
 
 
 app.get('/',(req,res)=>{
