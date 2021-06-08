c4becfc56b4475b391f50e37657219053d6ac903
Rishika0109 <82610555+Rishika0109@users.noreply.github.com>
2021-06-08 18:53:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const passport = require('passport');
+const router=require('./routes/auth_routes');
+const router1=require('./routes/payment_routes');
+const keys= require('./config/keys');
+const passportSetup = require('./config/passport-setup');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -11,6 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.use('/auth',router);
+app.use('/payment',router1);
+
+
+app.get('/',(req,res)=>{
+  res.send('sucess');
+});
 app.use("/api", authRoutes);
 
 app.listen(process.env.PORT || 2000, () => {
