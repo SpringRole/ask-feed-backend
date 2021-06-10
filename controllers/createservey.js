bcff9b5ae1cbf3bf225d268d6a31be45c172bd1b
Rishika0109 <82610555+Rishika0109@users.noreply.github.com>
2021-06-10 17:48:31 +0530

    Update createservey.js

--- controllers/createservey.js
+++ controllers/createservey.js
@@ -10,6 +10,27 @@ res.redirect('/');
         res.send(err);
     })
 });
+const invite_user = (req,res)=>{
+    const title=req.params.title;
+    Customer.findOne({title})
+    .then((data)=>{
+        var mailOptions={
+            from:'1802177.cse.cec@cgc.edu.in',
+            to:`${data.list}`,
+            subject:'regarding survey',
+            text:`http://localhost:7000/survey/${data._id}`
+        };
+        transporter.sendMail(mailOptions,(err,info)=>{
+            if(err)
+            {console.log(err);}
+            else
+            {res.send(info.response);}
+        })
+
+    })
+};
+
 module.exports ={
-  create_servey
+  create_servey,
+    invite_user
 };
