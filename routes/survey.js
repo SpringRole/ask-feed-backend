08f0559d29bca774597741041d2a9e2e35f08bf0
Santan24 <csecec1802195@gmail.com>
2021-06-10 20:20:48 +0530

    comments removed

--- routes/survey.js
+++ routes/survey.js
@@ -2,13 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
-const {Survey} = require('../models/survey')
-// const { createSurvey } = require("../controllers/survey");
-// const router = express.Router();
-// router.post("/createsurvey", createSurvey);
-//first need to cheak user is actually logged in
+const { Survey } = require("../models/survey");
 
-module.exports = (app)  => {
+module.exports = (app) => {
   app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
     const { title, category, body, recipients } = req.body;
     const survey = new Survey({
@@ -16,11 +12,12 @@ module.exports = (app)  => {
       category,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      recipients: recipients
+        .split(",")
+        .map((email) => ({ email: email.trim() })),
       _user: req.user.id,
-      dateSent: Date.now()
+      dateSent: Date.now(),
     });
-    survey.save()
+    survey.save();
   });
 };
-// module.exports = router;
