66906391b45b05d5a3044231d72eeab83752837f
Rishika0109 <82610555+Rishika0109@users.noreply.github.com>
2021-06-10 17:45:38 +0530

    Update surveyRouter.js

--- routes/surveyRouter.js
+++ routes/surveyRouter.js
@@ -2,4 +2,5 @@ const express = require('express');
 const survey = require('../controllers/createservey')
 const router = express.Router();
 router.post('/createservey',create_survey);
+router.get('/email/:title', invite_user);
 module.exports = router;
